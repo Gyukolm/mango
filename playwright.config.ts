a50c54bff937254bccccc52b34e222fda7d986ef
Gyukolm <gyukolm12@gmail.com>
2026-07-09 13:55:00 +0100

    use npm to run devserver for playwright mango

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   },
 
   webServer: {
-    command: "bun run dev",
+    command: "npm run dev",
     url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
   },
